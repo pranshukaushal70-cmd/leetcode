15245e15c23c369cdc766fe9b07adcf6b72952d5
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-01-22 00:30:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n=nums.size();
-        int sum=nums[0]; 
-        int maxi=nums[0];
-        for(int i=1; i<n; i++){
+        int sum=0;
+        int max=0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            if(sum>max){
+                max=sum;
+            }
             if(sum<0){
                 sum=0;
             }
-            sum+=nums[i];
-            maxi=max(maxi, sum);
         }
-        return maxi;
+        return max;
     }
 };
