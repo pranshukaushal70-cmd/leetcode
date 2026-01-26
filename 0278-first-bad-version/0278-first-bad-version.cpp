372c3519869cf1c2fed1e9183cea4cbfc456a463
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-01-27 02:38:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,21 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low=0;
+        int high=n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            bool p=isBadVersion(mid);
+            if(p == true){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+};
