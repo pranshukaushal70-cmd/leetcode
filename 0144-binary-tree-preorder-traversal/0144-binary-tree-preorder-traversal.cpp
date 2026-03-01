33e30abdeda45321ae125cdf964a90a71c686638
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-03-01 18:09:54 +0530

    Time: 0 ms (100%), Space: 10.9 MB (42.67%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,22 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        preorder(root, ans);
-        return ans;
-    }
-
-    void preorder(TreeNode* node, vector<int>& ans) {
-        if (!node) return;
+    
+    void preorder(TreeNode* root, vector<int>& result) {
+        if(root == NULL) return;
         
-        ans.push_back(node->val);        
-        preorder(node->left, ans);       
-        preorder(node->right, ans);      
+        result.push_back(root->val);   
+        preorder(root->left, result);  
+        preorder(root->right, result); 
+    }
+    
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> result;
+        preorder(root, result);
+        return result;
     }
 };
