87d7cc0e962426d1bb3303a576e19c7ca246bb65
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-06-06 23:55:06 +0530

    Time: 3 ms (18.81%), Space: 8.9 MB (53.86%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans="";
-        int count=0;
-        for(char c : s){
-            if(c == '('){
-                if(count > 0) ans+='(';
-                count++;
+        stack<char> st;
+        string ans;
+        int n=s.size();
+        for(int i=0; i<n; i++){
+            if(s[i] == ')'){
+                char a = st.top();
+                st.pop();
             }
-
-            else{
-                count--;
-                if(count > 0) ans+=')';
-            }
+            if(!st.empty()) ans.push_back(s[i]);
+            if(s[i] == '(') st.push(s[i]);
         }
         return ans;
     }
