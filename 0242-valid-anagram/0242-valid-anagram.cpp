e8f497face2db3412eefc26ed06afcd02df9d578
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-06-07 04:36:55 +0530

    Time: 8 ms (6.86%), Space: 9.5 MB (99.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()) return false;
-        
-        vector<int> count(26, 0);
-        
-        for(int i = 0; i < s.length(); i++) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
-        }
-        
-        for(int i = 0; i < 26; i++) {
-            if(count[i] != 0) return false;
-        }
-        
-        return true;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s == t;
     }
 };
