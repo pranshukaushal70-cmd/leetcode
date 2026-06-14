adce59955167ef9b9a7a65e0fc0ea3a5eddc244c
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-06-15 01:59:06 +0530

    Time: 0 ms (100%), Space: 31.4 MB (43.8%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -13,16 +13,7 @@ class Solution {
 public:
     int countNodes(TreeNode* root) {
         if (root == NULL) return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int count = 0;
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-            count++;
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
-        }
-        return count;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
