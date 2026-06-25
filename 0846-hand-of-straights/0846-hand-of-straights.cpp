ac88e00874f7a52c1472d93ec14acfb19307891a
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-06-26 00:55:22 +0530

    Time: 32 ms (78.27%), Space: 32.4 MB (41.18%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if (n % groupSize != 0)
-            return false;
-        unordered_map<int,int> freq;
-        for (int x : hand)
-            freq[x]++;
-        priority_queue<int,
-                       vector<int>,
-                       greater<int>> minHeap;
-        for (auto &it : freq)
-            minHeap.push(it.first);
-        while (!minHeap.empty()) {
-            int start = minHeap.top();
-            for (int i = 0; i < groupSize; i++) {
-                int card = start + i;
-                if (freq[card] == 0)
-                    return false;
-                freq[card]--;
-                if (freq[card] == 0) {
-                    if (card != minHeap.top())
-                        return false;
-                    minHeap.pop();
+        if(hand.size() % groupSize != 0 ) return false;
+        unordered_map<int,int> mp;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(auto x : hand){
+            mp[x]++;
+        }
+        for(auto x : mp) pq.push(x.first);
+        while(pq.size()){
+            int val = pq.top();
+            for(int i=0; i<groupSize; i++){
+                if(mp.find(i+val) == mp.end()) return false;
+                mp[i+val]--;
+                if(mp[i+val] == 0){
+                    pq.pop();
                 }
             }
         }
