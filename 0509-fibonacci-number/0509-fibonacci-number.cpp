31a4c888e727b9081977ed8e70720e7c93bf5d4a
Pranshu Kaushal <pranshukaushal70@gmail.com>
2026-07-17 05:25:48 +0530

    Time: 13 ms (32.27%), Space: 7.7 MB (65.21%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int fibonacci(int n){
+        if(n<=1) return n;
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    int fib(int n) {
+        int a=fibonacci(n);
+        return a;
+    }
+};
